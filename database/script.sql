11754c21d1d85e03d46a18c7b779cddd4af599b7
Neals Paye <neals.paye@tecsup.edu.pe>
2023-05-09 22:02:49 -0500

    tabla persona

--- database/script.sql
+++ database/script.sql
@@ -1,5 +1,5 @@
+create database crud;
 use crud;
-
 
 create table promociones(
 id int NOT NULL auto_increment,
@@ -10,3 +10,14 @@ primary key(id),
 key fk_promociones_1_idx (id_persona),
 constraint fk_promociones_1 foreign key (id_persona) references persona (id)
 );
+
+use crud;
+create table persona(
+id int auto_increment primary key,
+nombres varchar(100),
+apellido_paterno varchar(100),
+apellido_materno varchar(100),
+fecha_nacimiento date,
+celular varchar(12),
+);
+
