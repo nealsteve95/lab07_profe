c71c4676d8bd7d47ca610d144d4322a0c9f4b6bc
branyerbjr <branyer.romero@tecsup.edu.pe>
2023-05-09 22:24:19 -0500

    configurando el modo

--- database/script.sql
+++ database/script.sql
@@ -1,23 +1,24 @@
 create database crud;
 use crud;
 
-create table promociones(
-id int NOT NULL auto_increment,
-promocion varchar(200) default null,
-duracion varchar(200) default null,
-id_persona int not null,
-primary key(id),
-key fk_promociones_1_idx (id_persona),
-constraint fk_promociones_1 foreign key (id_persona) references persona (id)
+create table persona(
+    id int auto_increment primary key,
+    nombres varchar(100),
+    apellido_paterno varchar(100),
+    apellido_materno varchar(100),
+    fecha_nacimiento date,
+    celular varchar(12)
 );
 
-use crud;
-create table persona(
-id int auto_increment primary key,
-nombres varchar(100),
-apellido_paterno varchar(100),
-apellido_materno varchar(100),
-fecha_nacimiento date,
-celular varchar(12),
+create table promociones(
+    id int NOT NULL auto_increment,
+    promocion varchar(200) default null,
+    duracion varchar(200) default null,
+    id_persona int not null,
+    primary key(id),
+    key fk_promociones_1_idx (id_persona),
+    constraint fk_promociones_1 foreign key (id_persona) references persona (id)
 );
+
+
 
--- enviarMensaje.php
+++ enviarMensaje.php
@@ -15,7 +15,38 @@ $sentencia->execute([$codigo]);
 $persona = $sentencia->fetch(PDO::FETCH_OBJ);
 
     // Para que la imange se mande pero no descargue se usa el SendFileByUpload
-    
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.green-api.com/waInstance1101816201/sendFileByUpload/0e715d66ab8f4d53a5f8185a486b5cbb06c117a0168b493aaa",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => array(
+        'chatId' => "51".$persona->celular."@c.us",
+        'caption' => 'Estimado(a) *'.strtoupper($persona->nombres).' '.strtoupper($persona->apellido_paterno).' '.strtoupper($persona->apellido_materno).'* No se pierda *'.strtoupper($persona->promocion).'* valido solo *'.$persona->duracion.'*',
+        'file' => curl_file_create('Imagen/descarga.jpg', 'image/jpeg', 'descarga.jpg')
+    ),
+    CURLOPT_HTTPHEADER => array(
+        "cache-control: no-cache",
+        "content-type: multipart/form-data"
+    ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+    echo $response;
+    }
+    /* ********************** 
     $url = 'https://api.green-api.com/waInstance1101816201/sendFileByUpload/0e715d66ab8f4d53a5f8185a486b5cbb06c117a0168b493aaa';
 
     $data = array(  
@@ -53,9 +84,6 @@ $persona = $sentencia->fetch(PDO::FETCH_OBJ);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $files);
     
     $response = curl_exec($ch);
+    */
     
 ?> 
-
-
-
-
